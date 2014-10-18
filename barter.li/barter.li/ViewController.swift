fbf2a25c852873e58686e3c13193cb9bf03b2cbc
Aditya Sinha <aditya@Adityas-MacBook-Pro.local>
2014-10-18 17:17:43 +0530

    Project files.

---
+++ barter.li/barter.li/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  barter.li
+//
+//  Created by Aditya Sinha on 18/10/14.
+//  Copyright (c) 2014 barter.li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
