fe7289dba4f6ffdc2fef0f22a5666098798a0171
Shawn Noronha <shawn_noronha@icloud.com>
2015-01-18 16:10:07 +0530

    Added method for fetching API results data
    API data fetch

--- barter.li/barter.li/ViewController.m
+++ barter.li/barter.li/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+#define kListResultsForLocationCoordinates @"http://api.barter.li/api/v1/search.json?per=%ld&page=%ld&longitude=%.5lf&latitude=%.5lf"
 @end
 
 @implementation ViewController
@@ -17,11 +17,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self fetchBooksAtCurrentLocation];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void) fetchBooksAtCurrentLocation
+{
+    NSString *apiRequestString=[NSString stringWithFormat:kListResultsForLocationCoordinates, (long)0, (long)0,77.6276092,12.9399408];
+    
+    NSURLSessionDataTask *dataTask=[[NSURLSession sharedSession] dataTaskWithURL:[NSURL URLWithString:apiRequestString] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        
+        if(!error && data)
+        {
+            NSError *anError;
+            
+            NSDictionary *responseDict=[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&anError];
+            
+            NSLog(@"\n Response ==> %@", responseDict);
+            
+            
+        }
+    }];
+
+    
+    
+    [dataTask resume];
 }
 
 @end
