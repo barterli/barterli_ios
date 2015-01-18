968da17149ea0376286e67927b7c90862696c27f
Aditya Sinha <me@adityasinha.in>
2015-01-18 14:11:14 +0530

    1. Using Objective C -- Let Swift mature a bit. No time to beat around this. Old is Gold. 2. Universal app -- can be used on iPhone / iPad / iPod. 3. Core data -- data persistency will be the last priority and won't be shipped with 1.0.
    Let's Go!

---
+++ barter.li/barter.li/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Barter.Li
+//
+//  Created by Aditya Sinha on 1/18/15.
+//  Copyright (c) 2015 Barter.Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
