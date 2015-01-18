738d1a9ad9e207514d45d22ac3a429f23f48b5b0
Shawn Noronha <shawn_noronha@icloud.com>
2015-01-18 17:22:20 +0530

    Modal classes description string for debugging
    Modal classes description string for debugging

--- barter.li/barter.li/Modal Classes/BLBook/BLBook.m
+++ barter.li/barter.li/Modal Classes/BLBook/BLBook.m
@@ -10,4 +10,9 @@
 
 @implementation BLBook
 
+-(NSString*) description
+{
+    return [NSString stringWithFormat:@"\n\nTitle = %@, Author = %@, BookID = %@, thumbnailURL = %@, ISBN10 = %@, ISBN13 = %@ \nDescription = %@", _title, _author, _bookID, _coverImage, _ISBN10, _ISBN13, _bookDescription];
+}
+
 @end
--- barter.li/barter.li/Modal Classes/BLUser/BLUser.m
+++ barter.li/barter.li/Modal Classes/BLUser/BLUser.m
@@ -10,4 +10,9 @@
 
 @implementation BLUser
 
+-(NSString*) description
+{
+    return [NSString stringWithFormat:@"\n\n Name = %@, ImageURL = %@, userID = %@", _userName,_userImageURL,_userID];
+}
+
 @end
